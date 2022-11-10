7cc083272275ca5a3a3799d628780fa5a2b60015
Joshua Barrass <josh.barrass.work@gmail.com>
2022-11-10 14:51:12 +0000

    Allow direct input of sudoku if input is not a file

--- Main.hs
+++ Main.hs
@@ -3,12 +3,24 @@ import Display
 import Parse
 
 import System.Environment
+import System.Directory
+
+printSol :: Sudoku -> IO ()
+printSol puzzle = do
+  let sols = getSolutions puzzle
+  putStrLn $ prettyPrint $ head sols
 
 main :: IO ()
 main = do
   args <- getArgs
-  puzzleStr <- readFile (head args)
-  let puzzle = parseSudoku puzzleStr
-  let sols = getSolutions puzzle
-  putStrLn $ prettyPrint $ head sols
+  let filename = head args
+  fileExists <- doesFileExist filename
+  if fileExists then do
+    puzzleStr <- readFile (head args)
+    let puzzle = parseSudoku puzzleStr
+    printSol puzzle
+  else do
+    putStrLn "Warning: file does not exist; interpreting arg as a puzzle string\n"
+    let puzzle = parseSudoku filename
+    printSol puzzle
   
